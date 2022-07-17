b1344349d29d5a45617e58fb56d055b9cb31400c
Danilo de Sousa <danailus@gmail.com>
2022-07-17 10:16:49 -0300

    Mudando a forma de alertar o cliente dos erros

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ const pesquisarCep = async() => {
         const dados = await fetch(url);
         const endereco = await dados.json();
         if (endereco.hasOwnProperty('erro')){
-            document.getElementById('endereco').value = 'CEP não encontrado!';
+            alert('CEP não encontrado!');
         }else {
             preencherFormulario(endereco);
         }
     }else{
-        document.getElementById('endereco').value = 'CEP incorreto!';
+        alert('CEP incorreto!');;
     }
     
 }
